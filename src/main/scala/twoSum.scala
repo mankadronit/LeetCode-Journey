ccbb7ff465b0e9d529d7308c3654a50ea5f787d4
Ronit Mankad <mankadronit@users.noreply.github.com>
2024-04-07 17:14:01 -0400

    Update twoSum.scala

--- src/main/scala/twoSum.scala
+++ src/main/scala/twoSum.scala
@@ -1,5 +1,20 @@
-object Solution {
-  def main(args: Array[String]) = {
-    println("Hello World!")
+import scala.collection.mutable.{HashMap, ArrayBuffer}
+
+object Solution extends App {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    val hashMap = new HashMap[Int, Int]()
+    val results: ArrayBuffer[Int] = ArrayBuffer[Int]()
+
+    for ((element, index) <- nums.zipWithIndex) {
+      val complement = target - element
+      if (hashMap.contains(complement))
+        results.addOne(index)
+        results.addOne(hashMap(complement))
+      else {
+        hashMap.put(element, index)
+      }
+    }
+
+    results.toArray
   }
 }
